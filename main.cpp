4e238b154eede674a4c3751462b5dcda956146dd
ZaytsLinux <10kcin01gmail.com>
2025-03-01 13:25:51 +0500

    add srv ip address

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
-#include <filesystem>
+// #include <filesystem>
 #include <fstream>
 #include <iostream>
 // #include <istream>
 #include <chrono>
-#include <future>
+// #include <future>
 #include <math.h>
-#include <sstream>
+// #include <sstream>
 #include <string>
 #include <thread>
 #include <vector>
-
-#include <nlohmann/json.hpp>
+// #include <mutex>
 
 #include <prometheus/counter.h>
 #include <prometheus/exposer.h>
@@ -18,6 +17,8 @@
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
+
+#include <nlohmann/json.hpp>
 
 struct dataForRequest {
   dataForRequest(const std::string &_ip) : printerIP(_ip) {}
@@ -27,7 +28,8 @@ struct dataForRequest {
   std::string printerIP = "192.168.173.24";
 };
 
-#define PRINTER_NAME_OID ".1.3.6.1.2.1.1.5.0"
+// #define PRINTER_NAME_OID ".1.3.6.1.2.1.1.5.0"
+#define PRINTER_SN_OID "1.3.6.1.2.1.43.5.1.1.17.1"
 #define PRINTER_MODEL_OID ".1.3.6.1.2.1.25.3.2.1.3.1"
 #define PRINTED_PAGES_OID ".1.3.6.1.2.1.43.10.2.1.4.1.1"
 
@@ -65,6 +67,7 @@ struct tmpMetric {
   std::string ip = "0";
   std::string name = "0";
   std::string model = "0";
+  bool online = 0;
   //   std::string pages="0";
   int pages = 0;
 };
@@ -75,7 +78,7 @@ void process_snmp_response(netsnmp_pdu *response, tmpMetric &tmp) {
        vars = vars->next_variable) {
     // std::cout<<snmp_type_to_string(vars->type)<<std::endl;
     if (vars->type == ASN_OCTET_STR && firstTray) {
-      std::cout << "Model printer: "
+      std::cout << "SN printer: "
                 << std::string((char *)vars->val.string, vars->val_len)
                 << std::endl;
       tmp.model = std::string((char *)vars->val.string, vars->val_len);
@@ -90,6 +93,9 @@ void process_snmp_response(netsnmp_pdu *response, tmpMetric &tmp) {
       tmp.pages = *vars->val.integer;
     }
   }
+  if (tmp.pages != 0) {
+    tmp.online = 1;
+  }
 }
 
 tmpMetric queryPrinter(/*const*/ dataForRequest data) {
@@ -97,15 +103,15 @@ tmpMetric queryPrinter(/*const*/ dataForRequest data) {
   snmp_session *ss = create_snmp_session(data);
   netsnmp_pdu *pdu =
       snmp_pdu_create(SNMP_MSG_GET); // Создание PDU для SNMP-запроса
-  add_oid_to_pdu(pdu, PRINTER_NAME_OID);
+  add_oid_to_pdu(pdu, PRINTER_SN_OID);
   add_oid_to_pdu(pdu, PRINTER_MODEL_OID);
   add_oid_to_pdu(pdu, PRINTED_PAGES_OID);
 
   // Ответ от SNMP-сервера
   netsnmp_pdu *response = nullptr; // Инициализация на случай ошибки
   if (!ss) {
-    snmp_perror("snmp_open");   
-    tmp.ip="0"; 
+    snmp_perror("snmp_open");
+    tmp.ip = "0";
     return tmp; // return;
   }
   int status = snmp_synch_response(ss, pdu, &response);
@@ -130,15 +136,26 @@ void startPrometeus(std::vector<dataForRequest> &snmpAgents) {
   std::cout << "start" << std::endl;
   using namespace prometheus;
 
+  std::string srv_ip;
+  std::ifstream ipLists("ipLists.json");
+  if (ipLists.is_open()) {
+    nlohmann::json dict;
+    ipLists >> dict;
+    // if (dict["Srv_ip"]) {
+      srv_ip = dict["Srv_ip"];
+    // }
+  }
+  ipLists.close();
   // create an http server running on port 8080
-  Exposer exposer{"127.0.0.1:8080"};
+  Exposer exposer{srv_ip}; //{"192.168.173.10:8100"};
+  // Exposer exposer{"127.0.0.1:8080"};
   auto registry = std::make_shared<Registry>();
   exposer.RegisterCollectable(registry);
   auto start_time = std::chrono::steady_clock::now();
 
   // while (true) {
   while (std::chrono::steady_clock::now() - start_time <
-         std::chrono::seconds(60)) {
+         std::chrono::seconds(600)) {
     auto &pagesPriter = BuildGauge() // BuildCounter()
                             .Name("pages_printed")
                             .Help("number_of_pages_printed_on_a_printer")
@@ -150,17 +167,24 @@ void startPrometeus(std::vector<dataForRequest> &snmpAgents) {
     for (auto &snmpAgent : snmpAgents) {
       tmpMetric conpletedRequest = queryPrinter(snmpAgent);
       // if (conpletedRequest.name != "0" || conpletedRequest.model != "0") {
-        auto &printPag = pagesPriter.Add({{"ip_address", conpletedRequest.ip},
-                                          {"Name", conpletedRequest.name},
-                                          {"Model", conpletedRequest.model}});
-        // tmp = queryPrinter(b);  // Запрашиваем обновлённые данные
-        printPag.Set(
-            conpletedRequest.pages); // Устанавливаем значение в метрику
-        std::this_thread::sleep_for(
-            std::chrono::seconds(5)); // Обновляем раз в 5 секунд
+      std::string online;
+      if (conpletedRequest.online) {
+        online = "online";
+      } else {
+        online = "offline";
+      }
+
+      auto &printPag = pagesPriter.Add({{"Online", online},
+                                        {"ip_address", conpletedRequest.ip},
+                                        {"Name", conpletedRequest.name},
+                                        {"SN", conpletedRequest.model}});
+      // tmp = queryPrinter(b);  // Запрашиваем обновлённые данные
+      printPag.Set(conpletedRequest.pages); // Устанавливаем значение в метрику
+      std::this_thread::sleep_for(
+          std::chrono::seconds(5)); // Обновляем раз в 5 секунд
       // } else {
-        // auto &offlinePrinter = offlinePrinters.Add(
-        //     {"ip_address", conpletedRequest.ip}, {"status", "offline"});
+      // auto &offlinePrinter = offlinePrinters.Add(
+      //     {"ip_address", conpletedRequest.ip}, {"status", "offline"});
       // }
     }
   }
@@ -181,7 +205,7 @@ std::vector<dataForRequest> readTheListOfSnmpAgents() {
       for (auto &agent : dict["Snmp_Agent"]) {
         tmp.emplace_back(agent["community"], agent["printer_IP"]);
       }
-
+      ipLists.close();
       std::cout << "end read" << std::endl;
       return tmp;
     } else {
